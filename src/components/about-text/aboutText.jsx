01f5a864ff41de66998ebe072b958b081b5a563a
Neivo <126583090+VladislavXD@users.noreply.github.com>
2023-10-18 22:50:09 +0500

    Update aboutText.jsx

--- src/components/about-text/aboutText.jsx
+++ src/components/about-text/aboutText.jsx
@@ -1,13 +1,13 @@
-import { Fade } from "react-reveal"
+
 const AboutText = () => {
     return (
-        <Fade right cascade>
+        
             <div className="about__content_text">
                 <div className="about__content_title">Vladislav Dev</div>
                 <p className="about__content_decription">Good day, my name is Vladislav. I am an experienced FrontEnd developer with experience working with HTML, CSS, JavaScript. I have a passion for creating visually appealing and easy-to-use apps and websites that are both aesthetically pleasing and highly functional. I am very organized, attentive to details and diligent in my work, with a firm commitment to meeting deadlines. I am also an excellent communicator and have a proven ability to collaborate effectively with other developers, designers and other stakeholders. I always try to learn more and keep up to date with the latest trends and technologies.</p>
                 <div><a href="#" className='about__resume'>RESUME</a></div>
             </div>
-        </Fade>
+        
 
     )
 }
